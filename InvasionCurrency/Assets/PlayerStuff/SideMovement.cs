2ad696f984f3740dcdfed1b21465cd543d83f5e6
Tanner <thebow25@gmail.com>
2021-06-21 21:48:09 -0600

    Added move speed float values to the player

---
+++ InvasionCurrency/Assets/PlayerStuff/SideMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SideMovement : MonoBehaviour
+{
+
+	private CharacterController controller;
+	private float leftSpeed = 10f;
+	private float rightSpeed = 10f;
+	private float upSpeed = 5f;
+	private float downSpeed = 5f;
+	
+
+	void Start ()
+	{
+	controller = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
