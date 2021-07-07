4c69e33fbe316816178e1abc621f47e40a4a4ea5
Tanner <thebow25@gmail.com>
2021-07-06 23:31:40 -0600

    Made a triggerevent

---
+++ InvasionCurrency/Assets/PlayerStuff/Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    private UnityEvent healthDrain;
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthDrain.Invoke();
+    }
+}
