60d7f59911867adf271cad13459ee0d884178571
Tanner <thebow25@gmail.com>
2021-07-12 23:28:56 -0600

    Added a destroy script to targets.

---
+++ InvasionCurrency/Assets/Targets/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+	private float delay = 3f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, delay);
+	}
+	
+	// Update is called once per frame
+	private void OnDestroy()
+	{
+		Destroy(gameObject);
+	}
+}
