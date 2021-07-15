a950bff0ae1a3660b25bfdec9bcdaaa93758c402
Tanner <thebow25@gmail.com>
2021-07-14 23:14:01 -0600

    Targets can now destroy the player

--- InvasionCurrency/Assets/Targets/Destroy.cs
+++ InvasionCurrency/Assets/Targets/Destroy.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-	private float delay = 3f;
-
-	// Use this for initialization
-	void Start () {
-		Destroy(gameObject, delay);
-	}
 	
-	// Update is called once per frame
-	private void OnDestroy()
+	// Found this help on a form so im taking ideas on how to accomplish idea from it. https://stackoverflow.com/questions/37519034/falling-blocks-on-player-touch-unity-c
+	//
+	private GameObject player;
+	private bool playerEnter = false;
+
+	private void Awake()
 	{
-		Destroy(gameObject);
+		player = GameObject.Find("Player");
+	}
+
+public void Update()
+	{
+		if (player.transform.position.x == transform.position.x && player.transform.position.z == transform.position.z) //pretty much finding the player and entering the same cords
+		{
+			playerEnter = true; // saying if it enters the players position than destroy
+			Destroy(gameObject, 1f);
+		}
 	}
 }
