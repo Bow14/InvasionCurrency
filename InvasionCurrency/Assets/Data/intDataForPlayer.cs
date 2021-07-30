2a3886792f8ef82ee4227d827a16c5a6843dc5d6
Tanner <thebow25@gmail.com>
2021-07-29 22:51:14 -0600

    Organized and made int data scripts for objects

---
+++ InvasionCurrency/Assets/Data/intDataForPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intDataForPlayer : ScriptableObject
+{
+
+	public int value = 0;
+
+	public void Reset()
+	{
+		value = 0;
+	}
+
+	public void FixedUpdate(int number)
+	{
+		value += number;
+	}
+}
