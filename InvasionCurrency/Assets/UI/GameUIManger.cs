a9bcf56a53eff8956a8266ff402209b57f6f003f
Tanner <thebow25@gmail.com>
2021-07-31 20:41:30 -0600

    Has started my game manger ender UI

---
+++ InvasionCurrency/Assets/UI/GameUIManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManger : MonoBehaviour
+{
+
+	public bool gameEnded;
+
+	public GameObject gameEndedUI;
+
+	public void GameOver()
+	{
+		gameEndedUI.SetActive(true);
+	}
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	public void DeathScreen()
+	{
+		if (gameEnded)
+		{
+			gameEnded = true;
+			Debug.Log("Game Over");
+		}
+	}
+	}
