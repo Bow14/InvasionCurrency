d661a3218f4aeca712280aedd85ac184afb4c923
Tanner <thebow25@gmail.com>
2021-08-03 21:13:27 -0600

    Made a camera to follow player

--- InvasionCurrency/Assets/Data/intDataForPlayer.cs
+++ InvasionCurrency/Assets/Data/intDataForPlayer.cs
@@ -3,18 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class intDataForPlayer : ScriptableObject
+/*public class intDataForPlayer : ScriptableObject
 {
 
 	public int value = 0;
-
-	public void Reset()
-	{
-		value = 0;
-	}
-
-	public void FixedUpdate(int number)
-	{
-		value += number;
-	}
-}
+ 
+ 	public void Reset()
+ 	{
+ 		value = 0;
+ 	}
+ 
+ 	public void Update(int number)
+ 	{
+ 		value += number;
+ 	}
+ }*/
